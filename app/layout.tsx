837c790711bf56be6accc2a0c1d97773319b5a0f
peppeferd <giuseppeferlazzo@outlook.it>
2024-06-01 18:34:54 +0200

    New

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no">
       <body className={inter.className}>
         <Suspense fallback={<Loading />}>{children}</Suspense>
       </body>
