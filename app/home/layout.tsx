2191dcf135ae717482c20b37f4d5eb22bf11f9e4
peppeferd <giuseppeferlazzo@outlook.it>
2024-06-04 08:56:48 +0200

    New

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export const metadata = {
-  title: `A.G.A Ants' Golden Age - Home`,
-  description: 'Home',
+  title: `A.G.A - Ants' Golden Age`,
+  description: 'A.G.A',
 }
 
 const layout = ({ children }: { children: React.ReactNode }) => {
