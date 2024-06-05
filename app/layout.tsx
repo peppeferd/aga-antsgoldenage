a13b701ad8a575ded0ad6caf0522b694cedc938f
peppeferd <giuseppeferlazzo@outlook.it>
2024-06-05 10:21:47 +0200

    New one

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import './globals.css'
 import Loading from '@/components/Loading'
 import { Suspense } from 'react'
+import { Poppins as Inter } from 'next/font/google'
 
 export const metadata = {
   title: `A.G.A - Ants' Golden Age`,
   description: 'A.G.A',
 }
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+})
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -14,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" translate="no">
-      <body>
+      <body className={inter.className}>
         <Suspense fallback={<Loading />}>{children}</Suspense>
       </body>
     </html>
